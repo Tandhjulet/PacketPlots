f4e503cb0fba6e411630739608e00ba57bfe183b
Tandhjulet <madsbechmortensen@hotmail.dk>
2025-07-30 16:08:01 +0200

    patch: render pipelines are now fully operational

--- src/main/java/net/dashmc/plots/pipeline/transformers/BackdropAdderTransformer.java
+++ src/main/java/net/dashmc/plots/pipeline/transformers/BackdropAdderTransformer.java
@@ -1,12 +1,8 @@
 package net.dashmc.plots.pipeline.transformers;
 
-import java.util.Arrays;
-
 import lombok.RequiredArgsConstructor;
 import net.dashmc.plots.pipeline.IRenderTransformer;
 import net.dashmc.plots.plot.VirtualChunk;
-import net.dashmc.plots.plot.VirtualChunk.Section;
-import net.dashmc.plots.utils.Debug;
 import net.minecraft.server.v1_8_R3.Chunk;
 import net.minecraft.server.v1_8_R3.ChunkSection;
 import net.minecraft.server.v1_8_R3.PacketPlayOutMapChunk.ChunkMap;
@@ -18,7 +14,6 @@ public class BackdropAdderTransformer implements IRenderTransformer {
 	@Override
 	public void accept(VirtualChunk virtualChunk, ChunkMap chunkMap, Integer metaStartIdx) {
 		int mapPointer = 0;
-		Debug.log(Arrays.toString(backdropChunk.getSections()));
 
 		for (int i = 0; i < 16; i++) {
 			ChunkSection section = backdropChunk.getSections()[i];
@@ -32,13 +27,9 @@ public class BackdropAdderTransformer implements IRenderTransformer {
 			char[] blockIds = section.getIdArray();
 			for (int sectionPointer = 0; sectionPointer < blockIds.length; sectionPointer++, mapPointer += 2) {
 				char blockId = (char) ((chunkMap.a[mapPointer] & 0xFF) | ((chunkMap.a[mapPointer + 1] & 0xFF) << 8));
-				Debug.log("pointer is " + mapPointer + " (" + sectionPointer + ") with combined data "
-						+ Integer.valueOf(blockId) + "!");
-
-				if (blockId == (char) 0)
+				if (blockId != (char) 0)
 					continue;
 
-				// Debug.log("setting pos to 0");
 				char newBlock = blockIds[sectionPointer];
 				chunkMap.a[mapPointer] = (byte) (newBlock & 255);
 				chunkMap.a[mapPointer + 1] = (byte) ((newBlock >> 8) & 255);
