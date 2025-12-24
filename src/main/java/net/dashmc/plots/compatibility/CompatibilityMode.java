ce2f27fad9c7035d2584c651a22907d50513976b
Tandhjulet <madsbechmortensen@hotmail.dk>
2025-12-24 02:18:25 +0100

    feat: make compatibility modes injectable

--- src/main/java/net/dashmc/plots/compatibility/CompatibilityMode.java
+++ src/main/java/net/dashmc/plots/compatibility/CompatibilityMode.java
@@ -1,5 +1,9 @@
 package net.dashmc.plots.compatibility;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import lombok.Getter;
 import net.dashmc.plots.compatibility.compatibilities.ChunkMapSendCompatibility;
 import net.dashmc.plots.compatibility.compatibilities.HDBCompatibility;
@@ -10,13 +14,14 @@ public enum CompatibilityMode {
 	VULCAN(new VulcanCompatibility()),
 	FORCE_CHUNKMAP_SEND(new ChunkMapSendCompatibility()),
 	HEAD_DATABASE(new HDBCompatibility()),
-	NO_CHEAT_PLUS(new NCPCompatibility());
+	NO_CHEAT_PLUS(new NCPCompatibility()),
+	UNKNOWN();
 
 	@Getter
-	private final CompatibilityLoader[] loaders;
+	private final List<CompatibilityLoader> loaders;
 
 	CompatibilityMode(CompatibilityLoader... loaders) {
-		this.loaders = loaders;
+		this.loaders = new LinkedList<>(Arrays.asList(loaders));
 	}
 
 	public boolean shouldActivate() {
