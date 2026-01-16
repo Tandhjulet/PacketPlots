050f009aee13b2e2b47bde72233995524fc6b461
Tandhjulet <madsbechmortensen@hotmail.dk>
2026-01-16 08:29:57 +0100

    feat(paster): also copy tiles!

--- src/main/java/net/dashmc/plots/paste/Paster.java
+++ src/main/java/net/dashmc/plots/paste/Paster.java
@@ -2,17 +2,32 @@ package net.dashmc.plots.paste;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 
 import net.dashmc.plots.pipeline.RenderPipeline;
+import net.dashmc.plots.plot.IEnvironment;
 import net.dashmc.plots.plot.VirtualConnection;
 import net.dashmc.plots.plot.data.VirtualChunk;
 import net.dashmc.plots.utils.helpers.MethodWrapper;
 import net.dashmc.plots.utils.misc.Utils;
+import net.minecraft.server.v1_8_R3.Block;
+import net.minecraft.server.v1_8_R3.BlockPosition;
 import net.minecraft.server.v1_8_R3.Chunk;
 import net.minecraft.server.v1_8_R3.ChunkCoordIntPair;
 import net.minecraft.server.v1_8_R3.ChunkSection;
+import net.minecraft.server.v1_8_R3.EntityPlayer;
+import net.minecraft.server.v1_8_R3.IBlockData;
+import net.minecraft.server.v1_8_R3.IContainer;
+import net.minecraft.server.v1_8_R3.NBTTagCompound;
 import net.minecraft.server.v1_8_R3.NibbleArray;
+import net.minecraft.server.v1_8_R3.Packet;
 import net.minecraft.server.v1_8_R3.PacketPlayOutMapChunk;
+import net.minecraft.server.v1_8_R3.TileEntity;
 import net.minecraft.server.v1_8_R3.PacketPlayOutMapChunk.ChunkMap;
 import net.minecraft.server.v1_8_R3.WorldServer;
 
@@ -77,7 +92,57 @@ public class Paster {
 
 		nmsChunk.initLighting();
 
+		for (Packet<?> updatePacket : initializeTiles(chunk)) {
+			for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+				EntityPlayer nmsPlayer = ((CraftPlayer) player).getHandle();
+				nmsPlayer.playerConnection.sendPacket(updatePacket);
+			}
+		}
+
 		return true;
+	}
+
+	public static List<Packet<?>> initializeTiles(VirtualChunk vChunk) {
+		List<Packet<?>> updatePackets = new LinkedList<>();
+
+		IEnvironment environment = vChunk.getEnvironment();
+		Chunk chunk = vChunk.getChunk();
+
+		ChunkCoordIntPair vOffset = vChunk.getCoordPair();
+
+		int offsetX = chunk.locX;
+		int offsetZ = chunk.locZ;
+
+		for (ChunkSection section : chunk.getSections()) {
+			char[] ids = section.getIdArray();
+			for (int idx = 0; idx < ids.length; idx++) {
+				char id = ids[idx];
+				IBlockData ibd = Block.d.a(id);
+				Block block = ibd.getBlock();
+
+				if (!block.isTileEntity())
+					continue;
+
+				int x = (idx & 0xF);
+				int y = (idx >> 8) & 0xFF;
+				int z = ((idx >> 4) & 0xF);
+
+				TileEntity virtualizedTile = environment
+						.getTileEntity(
+								new BlockPosition(x + vOffset.x, y, z + vOffset.z));
+
+				NBTTagCompound compound = new NBTTagCompound();
+				virtualizedTile.b(compound);
+
+				TileEntity tile = ((IContainer) block).a(chunk.world, id);
+				tile.a(compound);
+
+				chunk.a(new BlockPosition(x + offsetX, y, z + offsetZ), tile);
+				updatePackets.add(tile.getUpdatePacket());
+			}
+		}
+
+		return updatePackets;
 	}
 
 	static {
